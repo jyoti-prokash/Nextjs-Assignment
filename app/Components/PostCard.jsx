b2e9383a0232615c81e94044acd73f3bfca31926
jyotiprokash <jyotiprokash2000@gmail.com>
2025-01-24 15:07:07 +0600

    final setup

--- app/Components/PostCard.jsx
+++ app/Components/PostCard.jsx
@@ -1,18 +1,30 @@
-import Link from 'next/link';
-import React from 'react';
+import {
+  LoginLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
+import React from "react";
 
-const PostCard = ({id,title,body}) => {
-    return (
-      <div className="card card-compact bg-base-100  shadow-xl hover:scale-52">
-        <div className="card-body">
-          <h2 className="card-title">{title}</h2>
-          <p>{body}</p>
-          <div className="card-actions justify-end">
-            <Link href={`DetailsPost/${id}`} className="btn btn-primary">Details</Link>
-          </div>
+const PostCard = async ({ id, title, body }) => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  return (
+    <div className="card card-compact bg-base-100  shadow-xl hover:scale-52">
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p>{body}</p>
+        <div className="card-actions justify-end">
+          {user ? (
+            <Link href={`DetailsPost/${id}`} className="btn btn-primary">
+              Details
+            </Link>
+          ) : (
+            <LoginLink className="btn btn-primary">Details</LoginLink>
+          )}
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default PostCard;
